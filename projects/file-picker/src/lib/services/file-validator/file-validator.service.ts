75e8661cb1df27229de42af6e0dc6fc2c6e5f33c
vugar <vugar_abdullayev@outlook.com>
2023-01-22 15:56:14 +0100

    add file validation service

---
+++ projects/file-picker/src/lib/services/file-validator/file-validator.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { FilePreviewModel } from 'file-picker';
+import { bitsToMB } from '../../file-picker.constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileValidatorService {
+
+  constructor() { }
+
+  /** Validates file extension */
+  public isValidExtension(fileName: string, fileExtensions: string[]): boolean {
+    if (!fileExtensions?.length) { return true; }
+
+    const extension: string = fileName.split('.').pop();
+    const fileExtensionsLowercase = fileExtensions.map(ext => ext.toLowerCase());
+    if (fileExtensionsLowercase.indexOf(extension.toLowerCase()) === -1) {
+      return false;
+    }
+    return true;
+  }
+
+  /** Validates if upload type is single so another file cannot be added */
+  public isValidUploadType(files: FilePreviewModel[], uploadType: string): boolean {
+    if (uploadType === 'single' && files?.length > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  /** Validates max file count */
+  public isValidMaxFileCount(fileMaxCount: number, newFiles: File[], files: FilePreviewModel[]): boolean {
+    if (!fileMaxCount || fileMaxCount >= files?.length + newFiles?.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public isValidFileSize(size: number, fileMaxSize: number) {
+    const fileMB: number = bitsToMB(size);
+    if (!fileMaxSize || (fileMaxSize && fileMB < fileMaxSize)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public isValidTotalFileSize(newFile: File, files: FilePreviewModel[], totalMaxSize: number) {
+    /** Validating Total Files Size */
+    const totalBits = files
+      .map(f => f.file ? f.file.size : 0)
+      .reduce((acc, curr) => acc + curr, 0);
+
+    if (!totalMaxSize || (totalMaxSize && bitsToMB(totalBits + newFile.size) < totalMaxSize)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
