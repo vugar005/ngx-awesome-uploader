5509bbaadd0d91c30a0b91dd916a7d67502508e2
vugar <28726265+vugar005@users.noreply.github.com>
2024-02-22 16:31:25 +0100

    add eslint and prettier

--- src/app/demo-file-picker/demo-file-picker.adapter.ts
+++ src/app/demo-file-picker/demo-file-picker.adapter.ts
@@ -11,38 +11,38 @@ export class DemoFilePickerAdapter extends FilePickerAdapter {
   public uploadFile(fileItem: FilePreviewModel): Observable<UploadResponse | undefined> {
     const form = new FormData();
     form.append('file', fileItem.file);
-   const api = 'https://ngx-awesome-uploader-2.free.beeceptor.com/upload';
-    const req = new HttpRequest('POST', api, form, {reportProgress: true});
-    return this.http.request(req)
-    .pipe(
-    // @ts-ignore: not all return value
-    // TODO: fix return type
+    const api = 'https://ngx-awesome-uploader-2.free.beeceptor.com/upload';
+    const req = new HttpRequest('POST', api, form, { reportProgress: true });
+    return this.http.request(req).pipe(
+      // @ts-expect-error: not all return value
+      // TODO: fix return type
       map((res: HttpEvent<any>) => {
         if (res.type === HttpEventType.Response) {
           const responseFromBackend = res.body;
           return {
             body: responseFromBackend,
-            status: UploadStatus.UPLOADED
+            status: UploadStatus.UPLOADED,
           };
-        } else if ((res.type ===  HttpEventType.UploadProgress) && res.total) {
+        } else if (res.type === HttpEventType.UploadProgress && res.total) {
           /** Compute and show the % done: */
-            const uploadProgress = +Math.round((100 * res.loaded) / res.total);
-            return {
-              status: UploadStatus.IN_PROGRESS,
-              progress: uploadProgress
-            };
+          const uploadProgress = +Math.round((100 * res.loaded) / res.total);
+          return {
+            status: UploadStatus.IN_PROGRESS,
+            progress: uploadProgress,
+          };
         }
       }),
       catchError((er: HttpErrorResponse) => {
         console.log(er);
-        return of({status: UploadStatus.ERROR, body: er });
+        return of({ status: UploadStatus.ERROR, body: er });
       })
-      );
+    );
   }
-    public removeFile(fileItem: FilePreviewModel): Observable<any> {
-      const id = 50;
-      const responseFromBackend = fileItem.uploadResponse;
-      const removeApi = 'https://run.mocky.io/v3/dedf88ec-7ce8-429a-829b-bd2fc55352bc';
-      return this.http.post(removeApi, {id});
-    }
+  public removeFile(fileItem: FilePreviewModel): Observable<any> {
+    const id = 50;
+    const responseFromBackend = fileItem.uploadResponse;
+    console.log(responseFromBackend);
+    const removeApi = 'https://run.mocky.io/v3/dedf88ec-7ce8-429a-829b-bd2fc55352bc';
+    return this.http.post(removeApi, { id });
+  }
 }
