2299d3f1f630ee372a9b2ce2b9e354eacff7a0f8
Vugar <inbox@vugar.app>
2024-12-23 13:06:22 -0800

    feature/upgrade-angular-v18 (#129)
    * upgrade angular v18
    
    * upgrade library package json
    
    ---------
    
    Co-authored-by: vugar <inbox@vugar.app>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DemoFilePickerComponent } from './demo-file-picker/demo-file-picker.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FilePickerModule } from 'projects/file-picker/src/public_api';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    DemoFilePickerComponent
-  ],
-  imports: [
-    BrowserModule,
-    FilePickerModule,
-    HttpClientModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        DemoFilePickerComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FilePickerModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
