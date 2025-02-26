4ac07be9a00cb41e63b0e13cb1cf2d0b14039829
vugar <28726265+vugar005@users.noreply.github.com>
2025-02-26 16:13:16 +0100

    upgrade angular to v19

--- projects/file-picker/src/lib/file-preview-container/file-preview-item/refresh-icon/refresh-icon.component.ts
+++ projects/file-picker/src/lib/file-preview-container/file-preview-item/refresh-icon/refresh-icon.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'refresh-icon',
-  templateUrl: './refresh-icon.component.html',
-  styleUrls: ['./refresh-icon.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'refresh-icon',
+    templateUrl: './refresh-icon.component.html',
+    styleUrls: ['./refresh-icon.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: false
 })
 export class RefreshIconComponent {
 
--- projects/file-picker/src/lib/icons/cloud-icon/cloud-icon.component.ts
+++ projects/file-picker/src/lib/icons/cloud-icon/cloud-icon.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'cloud-icon',
-  templateUrl: './cloud-icon.component.html',
-  styleUrls: ['./cloud-icon.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'cloud-icon',
+    templateUrl: './cloud-icon.component.html',
+    styleUrls: ['./cloud-icon.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: false
 })
 export class CloudIconComponent implements OnInit {
 
